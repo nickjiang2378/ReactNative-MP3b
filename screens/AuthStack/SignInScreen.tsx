811d33947a43121857abd50ed099e9869c6a362d
nickjiang2378 <41227221+nickjiang2378@users.noreply.github.com>
2021-10-19 22:40:15 -0700

    features working

---
+++ screens/AuthStack/SignInScreen.tsx
@@ -0,0 +1,160 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { View, SafeAreaView, StyleSheet, ScrollView, Text } from "react-native";
+import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
+import { AuthStackParamList } from "./AuthStackScreen";
+import firebase from "firebase";
+import { AppStyles } from "../../AppStyles";
+
+interface Props {
+  navigation: StackNavigationProp<AuthStackParamList, "SignInScreen">;
+}
+
+export default function SignInScreen({ navigation }: Props) {
+  /* Screen Requirements:
+      - AppBar
+      - Email & Password Text Input
+      - Submit Button
+      - Sign Up Button (goes to Sign Up screen)
+      - Reset Password Button
+      - Snackbar for Error Messages
+  
+    All UI components on this screen can be found in:
+      https://callstack.github.io/react-native-paper/
+
+    All authentication logic can be found at:
+      https://firebase.google.com/docs/auth/web/starts
+  */
+
+  const [email, setEmail] = useState<String>("");
+  const [password, setPassword] = useState<String>("");
+  const [errorMessage, setErrorMessage] = useState<String>("");
+  const [errorVisible, setErrorVisible] = useState<Boolean>(false);
+  const [loadingVisible, setLoadingVisible] = useState<Boolean>(false);
+
+  function loginUser(email: String, password: String) {
+    if (validateFields(email, password)) {
+      setLoadingVisible(true)
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        setLoadingVisible(false)
+        console.log("Successfully signed in")
+        var user = userCredential.user;
+      })
+      .catch((error) => {
+        setLoadingVisible(false)
+        var errorCode = error.code;
+        var error = error.message;
+        console.log(error)
+        if (errorCode == "auth/wrong-password") {
+          setErrorMessage("Email/Password credentials are invalid");
+          setErrorVisible(true);
+        }
+        console.log(errorCode);
+      });
+    }
+    else {
+      console.log("Validation failed")
+    }
+
+  }
+
+  function resetPassword(email: String) {
+    if (!email) {
+      showError("Please enter a valid email address.")
+    }
+    else {
+      firebase.auth().sendPasswordResetEmail(email)
+      .then(() => {
+        console.log("Reset password email sent!")
+        showError("Reset password email sent!")
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+        console.log(errorCode)
+        if (errorCode == "auth/invalid-email") {
+          showError("Please enter a valid email address.")
+        }
+      });
+    }
+    
+  }
+
+  function showError(mes: String) {
+    setErrorVisible(true)
+    setErrorMessage(mes)
+  }
+
+  function validateFields(email: String, password: String) {
+    if (!email) {
+      setErrorMessage("The email field is required")
+      setErrorVisible(true)
+    }
+    else if (!password) {
+      setErrorMessage("The password field is required")
+      setErrorVisible(true)
+    }
+    else {
+      return true
+    }
+  }
+
+
+  return (
+    <>
+      <SafeAreaView style={AppStyles.container}>
+        <Appbar.Header>
+          <Appbar.Content title="Sign In" />
+        </Appbar.Header>
+        <View>
+          <View style={{"padding": 20, "marginBottom": 20}}>
+          <TextInput 
+          label="Email"
+          style={AppStyles.text_input}
+          value={email}
+          onChangeText={text => setEmail(text)}
+          />
+          <TextInput 
+            label="Password"
+            style={AppStyles.text_input}
+            value={password}
+            onChangeText={text => setPassword(text)}
+            secureTextEntry={true}
+          />
+          <Button
+            mode="contained"
+            style={AppStyles.button}
+            loading={loadingVisible}
+            onPress={() => {console.log("Signing in user"); loginUser(email, password)}}
+          >
+            Sign In
+          </Button>
+          <Button
+            style={AppStyles.button}
+            onPress={() => {navigation.navigate("SignUpScreen")}}
+          >
+            Or, sign up instead
+          </Button> 
+          <Button
+            style={AppStyles.button}
+            onPress={() => {console.log("Resetting password"); resetPassword(email)}}
+          >
+            <Text style={AppStyles.faded_button}>Reset Password</Text>
+          </Button> 
+          </View>
+        </View>
+        <View style={{"margin": 10}}>
+          <Snackbar
+            visible={errorVisible}
+          >
+            {errorMessage}
+          </Snackbar>
+        </View>
+        
+      </SafeAreaView>
+    </>
+  );
+}
---
+++ screens/AuthStack/SignUpScreen.tsx
@@ -0,0 +1,135 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
+import { AuthStackParamList } from "./AuthStackScreen";
+import firebase from "firebase";
+
+import { AppStyles } from "../../AppStyles";
+
+interface Props {
+  navigation: StackNavigationProp<AuthStackParamList, "SignUpScreen">;
+}
+
+export default function SignUpScreen({ navigation }: Props) {
+  /* Screen Requirements:
+      - AppBar
+      - Email & Password Text Input
+      - Submit Button
+      - Sign In Button (goes to Sign In Screen)
+      - Snackbar for Error Messages
+  
+    All UI components on this screen can be found in:
+      https://callstack.github.io/react-native-paper/
+
+    All authentication logic can be found at:
+      https://firebase.google.com/docs/auth/web/start
+  */
+  const [email, setEmail] = useState<String>("")
+  const [password, setPassword] = useState<String>("")
+  const [errorMessage, setErrorMessage] = useState<String>("")
+  const [errorVisible, setErrorVisible] = useState<Boolean>(false)
+
+  function createUser(email: String, password: String) {
+    if (validateFields(email, password)) {
+      console.log("Attempting to login")
+      let auth = firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var error = error.message;
+        console.log(error)
+        if (errorCode == "auth/email-already-in-use") {
+          showError(error)
+        }
+        else if (errorCode == "auth/invalid-email") {
+          showError("Please enter a valid email address")
+        }
+        else if (errorCode == "auth/weak-password") {
+          showError("Please enter a stronger password")
+        }
+      });
+    }
+    else {
+      console.log("Fields invalid")
+    }
+  }
+
+  function showError(mes: String) {
+    setErrorVisible(true)
+    setErrorMessage(mes)
+  }
+
+  function validateFields(email: String, password: String) {
+    if (!email) {
+      setErrorMessage("The email field is required")
+      setErrorVisible(true)
+    }
+    else if (!password || password.length < 6) {
+      setErrorMessage("A password at least six characters long is required")
+      setErrorVisible(true)
+    }
+    else {
+      return true
+    }
+  }
+  
+  return (
+    <>
+      <SafeAreaView style={AppStyles.container}>
+        <Appbar.Header>
+          <Appbar.Content title="Sign Up" />
+        </Appbar.Header>
+        <View>
+          <View style={{"padding": 20, "marginBottom": 20}}>
+            <TextInput 
+              label="Email"
+              value={email}
+              style={AppStyles.text_input}
+              onChangeText={text => setEmail(text)}
+            />
+            <TextInput 
+              label="Password"
+              value={password}
+              style={AppStyles.text_input}
+              onChangeText={text => setPassword(text)}
+              secureTextEntry={true}
+            />
+            <View style={{"marginTop": 20}}>
+              <Button
+                mode="contained"
+                style={AppStyles.button}
+                onPress={() => {console.log("Signing up user"); createUser(email, password)}}
+              >
+                Create an account
+              </Button>
+              <Button
+                style={AppStyles.button}
+                onPress={() => {navigation.navigate("SignInScreen")}}
+              >
+                Or, sign in instead
+              </Button>
+            </View>
+          </View>
+          <View style={{"margin": 10}}>
+            <Snackbar
+              visible={errorVisible}
+              duration={700}
+            >
+              {errorMessage}
+            </Snackbar>
+          </View>
+            
+          
+
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
